25f7b5d37240e6707aa20ee3879ea39996bc68cd
welsper53 <welsper53@gmail.com>
2023-01-06 18:30:01 +0900

    23.01.06 프로토콜 설정

--- app/src/main/java/banana_project/server/Protocol.java
+++ app/src/main/java/banana_project/server/Protocol.java
@@ -5,94 +5,61 @@ public class Protocol {
     public static final String separator = "#";
 
     //          [로그인]           //
-    public static final int CLIENT_START = 100;
-    public static final int LOGIN_S = 101;
-    public static final int WRONG_ID = 102;
-    public static final int WRONG_PW = 103;
+    public static final int CLIENT_START = 100;     // 클라이언트 시작
+    public static final int LOGIN_S = 101;          // 로그인 성공
+    public static final int WRONG_ID = 102;         // 아이디가 틀림
+    public static final int WRONG_PW = 103;         // 패스워드 틀림
 
     //          [회원가입]           //
-    public static final int SIGN_IN = 200;
-    public static final int SIGN_COMPLETE = 201;
-    public static final int MAIL_CHK = 202;
-    public static final int MAIL_DUP_EXIT = 203;
-    public static final int NICK_CHK = 204;
-    public static final int NICK_DUP_EXIT = 205;
-    public static final int SIGN_SUS = 206;
-    public static final int SIGN_ERR = 207;
+    public static final int SIGN_IN = 200;          // 회원가입 시작
+    public static final int SIGN_COMPLETE = 201;    // 가입 완료
+    public static final int MAIL_CHK = 202;         // 이메일 중복 체크
+    public static final int MAIL_DUP_EXIT = 203;    // 이메일 중복 존재
+    public static final int NICK_CHK = 204;         // 닉네임 중복 체크
+    public static final int NICK_DUP_EXIT = 205;    // 닉네임 중복 존재
+    public static final int SIGN_SUS = 206;         // 회원가입 성공
+    public static final int SIGN_ERR = 207;         // 회원가입 실패
 
     //          [아이디 찾기]           //
-    public static final int FID_START = 300;
-    public static final int FID_EXIT = 301;
-    public static final int NOT_FID = 302;
-//    public static final int  = 303
-//    public static final int  = 304
-//    public static final int  = 305
-//    public static final int  = 306
-//    public static final int  = 307
+    public static final int FID_START = 300;        // 아이디찾기 시작
+    public static final int FID_EXIT = 301;         // 아이디찾기 종료
+    public static final int NOT_FID = 302;          // 아이디 존재하지 않음
+//    public static final int  = 30
 
     //          [비밀번호 찾기]           //
-    public static final int FPW_START = 400;
-    public static final int FPW_EXIT = 401;
-    public static final int NOF_ACNT = 402;
-    public static final int RESET_PW = 403;
-//    public static final int  = 404;
-//    public static final int  = 405;
-//    public static final int  = 406;
-//    public static final int  = 407;
-//    public static final int  = 408;
-//    public static final int  = 409;
-//    public static final int  = 410;
-//    public static final int  = 411;
-//    public static final int  = 412;
-//    public static final int  = 413;
-//    public static final int  = 414;
-//    public static final int  = 415;
+    public static final int FPW_START = 400;        // 비밀번호찾기 시작
+    public static final int FPW_EXIT = 401;         // 비밀번호찾기 종료
+    public static final int NOF_ACNT = 402;         // 계정 존재하지 않음
+    public static final int RESET_PW = 403;         // 비밀번호 재설정
+//    public static final int  = 40;
 
-    //          [친구 목록]           //
-    public static final int PRI_FRIENDS = 500;
-    public static final int MYPAGE_EV = 501;
-    public static final int ADD_FRIENDS_EV = 502;
-    public static final int NOF_FRIENDS = 503;
-//    public static final int  = 504;
-//    public static final int  = 505;
-//    public static final int  = 506;
-//    public static final int  = 507;
-//    public static final int  = 508;
-//    public static final int  = 509;
-//    public static final int  = 510;
-//    public static final int  = 511;
-//    public static final int  = 512;
-//    public static final int  = 513;
-//    public static final int  = 514;
+    //          [리스트 화면]           //
+    public static final int PRI_CHATS = 500;        // 채팅리스트 출력
+    public static final int NOF_CHATS = 501;        // 채팅리스트 없음
+    public static final int PRI_FRIENDS = 502;      // 친구목록 출력
+    public static final int NOF_FRIENDS = 503;      // 친구리스트 없음
+    public static final int BTN_MYPAGE = 504;       // 마이페이지 버튼 클릭
+    public static final int BTN_ADFRIEND = 505;     // 친구추가 버튼 클릭
+    public static final int BTN_NEWCHAT = 506;      // 새채팅 버튼 클릭
+    public static final int BTN_CHAT= 507;          // 채팅방 버튼 클릭
+    public static final int BTN_FRIENDS = 508;      // 친구리스트 버튼 클릭
+    public static final int LI_CHAT = 509;          // 채팅방 더블클릭
+    public static final int LI_FRIEND = 510;        // 친구 더블클릭
+//    public static final int  = 50;
 
     //          [친구 검색]           //
-//    public static final int PRIN = 600;
-//    public static final int  = 601;
-//    public static final int  = 602;
-//    public static final int  = 603;
-//    public static final int  = 604;
-//    public static final int  = 605;
-//    public static final int  = 606;
-//    public static final int  = 607;
-//    public static final int  = 608;
-//    public static final int  = 609;
-//    public static final int  = 610;
-//    public static final int  = 611;
-//    public static final int  = 612;
-//    public static final int  = 613;
-//    public static final int  = 614;
+    public static final int PRINT_FRIENDS = 600;    // 친구검색 출력
+    public static final int SCH_FRIENDS = 601;      // 검색버튼 이벤트
+    public static final int NOF_RESULT = 602;       // 검색 결과가 없음
+    public static final int ADD_FRIENDS = 603;      // 친구 추가 이벤트
+    public static final int CREATE_CHAT = 604;      // 채팅방 만들기 이벤트
+//    public static final int  = 60;
 
 
     //          [대화방 화면]           //
+    public static final int CHAT_START = 700;       // 대화방화면 출력
+    public static final int P_MESSAGE = 701;        // 1:1 대화 메세지
+    public static final int G_MESSAGE = 702;        // 단체 대화 메세지
+//    public static final int  = 70;
 
-
-
-
-
-
-//    public static final int TALK_IN = 100;
-//    public static final int MESSAGE = 200;
-//    public static final int WHISPER = 300;
-//    public static final int CHANGE = 400;
-//    public static final int TALK_OUT = 500;
 }
