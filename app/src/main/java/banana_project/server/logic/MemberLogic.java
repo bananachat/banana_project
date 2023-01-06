3041b3df45ef401bd64caf13f74212d8e0e755cd
77EJ77 <temp221128@gmail.com>
2023-01-06 17:51:26 +0900

    01/06 memberlogin update

--- app/src/main/java/banana_project/server/logic/MemberLogic.java
+++ app/src/main/java/banana_project/server/logic/MemberLogic.java
@@ -1,38 +1,45 @@
-//package banana_project.server.logic;
-//
-//import banana_project.server.util.DBConnectionMgr;
-//import banana_project.server.vo.UserVO;
-//
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//
-//public class MemberLogic {
-//
-//    DBConnectionMgr mgr = new DBConnectionMgr();
-//    Connection con = null;
-//    PreparedStatement pst = null;
-//    ResultSet rs = null;
-//
-//
-//    public MemberLogic(){
-//
-//    }
-//
-//    public int joinUser(UserVO uservo){
-//
-//        int result = -1;
-//        StringBuilder sql = new StringBuilder();
-//        S
-//        try {
-//            con = mgr.getConnection();
-//            pst = con.prepareStatement(sql.toString());
-//
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return result;
-//    }
-//
-//}
+package banana_project.server.logic;
+
+import banana_project.server.util.DBConnectionMgr;
+import banana_project.server.vo.UserVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberLogic {
+
+    DBConnectionMgr mgr = new DBConnectionMgr();
+    Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+
+    /**
+     * MemberLogic constructor
+     */
+    public MemberLogic(){
+
+    }
+
+    /**
+     * 회원가입 INSERT
+     *
+     * @param uservo
+     * @return result
+     */
+    public int joinUser(UserVO uservo){
+
+        int result = -1;
+        StringBuilder sql = new StringBuilder();
+
+        try {
+            con = mgr.getConnection();
+            pst = con.prepareStatement(sql.toString());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
