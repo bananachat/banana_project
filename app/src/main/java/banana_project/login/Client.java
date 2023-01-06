8b27adc531a5a346cf9e389651202d2311c1ddc5
jungyunmok <jungyunmok@gmail.com>
2023-01-06 17:31:25 +0900

    01/06 안내창추가

--- app/src/main/java/banana_project/login/Client.java
+++ app/src/main/java/banana_project/login/Client.java
@@ -4,10 +4,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import javax.swing.plaf.ColorUIResource;
 import java.awt.Color;
 import java.awt.Font;
@@ -15,13 +16,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class Client extends JFrame implements ActionListener, MouseListener {
+public class Client extends JFrame implements ActionListener, MouseListener, FocusListener {
   // 클래스 연결
   // Main main = null; //메인화면 연결
   // Memjoin memjoin = null; //회원가입 연결
@@ -38,21 +39,11 @@ public class Client extends JFrame implements ActionListener, MouseListener {
   // 화면부 선언
   JFrame jf_login = new JFrame(); // 메인 프레임
   JPanel jp_login = new JPanel(null); // 로그인 패널
-  JLabel jlb_idText = new JLabel("  example@email.com"); // jtf위에 표시될 jlb
-  JLabel jlb_pwText = new JLabel("  password"); // jtf위에 표시될 jlb
   JLabel jlb_findId = new JLabel(); // 아이디찾기 라벨
   JLabel jlb_findPw = new JLabel(); // 비밀번호 찾기 라벨
   // 아이디, 비밀번호 입력을 위한 JTextField (테두리선을 지우기위해 클래스 재정의)
-  JTextField jtf_userId = new JTextField() { // 아이디 입력창
-    @Override
-    public void setBorder(Border border) {
-    }
-  };
-  JPasswordField jtf_userPw = new JPasswordField() { // 비밀번호 입력창
-    @Override
-    public void setBorder(Border border) {
-    }
-  };
+  JTextField jtf_userId = new JTextField(" example@email.com"); // 아이디 입력창
+  JPasswordField jtf_userPw = new JPasswordField(" password"); // 비밀번호 입력창
   // 폰트 설정
   Font p12 = new Font("맑은 고딕", Font.PLAIN, 12); // 보통 폰트
   Font b14 = new Font("맑은 고딕", Font.BOLD, 14); // 볼드 폰트
@@ -72,15 +63,40 @@ public class Client extends JFrame implements ActionListener, MouseListener {
     // 이벤트리스너 연결
     jbtn_login.addActionListener(this);
     jbtn_join.addActionListener(this);
-    // 로그인, 패스워드 라벨 설정
-    jlb_idText.setForeground(Color.gray);
-    jlb_pwText.setForeground(Color.gray);
-    jlb_idText.setBounds(60, 300, 270, 45);
-    jlb_pwText.setBounds(60, 340, 270, 45);
-    jlb_idText.setFont(p12);
-    jlb_pwText.setFont(p12);
-    jp_login.add(jlb_idText);
-    jp_login.add(jlb_pwText);
+    jtf_userId.addActionListener(this);
+    jtf_userPw.addActionListener(this);
+    jtf_userId.addFocusListener(this);
+    jtf_userPw.addFocusListener(this);
+    jlb_findId.addMouseListener(this);
+    jlb_findPw.addMouseListener(this);
+
+    // 힌트문 테스트
+    // jtf_userId.addFocusListener(new FocusAdapter() {
+    // @Override
+    // public void focusGained(FocusEvent e) {
+    // if (getText().equals(hint)) {
+    // setText("");
+    // setFont(gainFont);
+    // } else {
+    // setText(getText());
+    // setFont(gainFont);
+    // }
+    // }
+
+    // @Override
+    // public void focusLost(FocusEvent e) {
+    // if (getText().equals(hint) || getText().length() == 0) {
+    // setText(hint);
+    // setFont(lostFont);
+    // setForeground(Color.GRAY);
+    // } else {
+    // setText(getText());
+    // setFont(gainFont);
+    // setForeground(Color.BLACK);
+    // }
+    // }
+    // });
+
     // 패널에 추가
     jp_login.add(jtf_userId);
     jp_login.add(jtf_userPw);
@@ -89,32 +105,34 @@ public class Client extends JFrame implements ActionListener, MouseListener {
     jp_login.add(jlb_findId);
     jp_login.add(jlb_findPw);
     jp_login.add(jbtn_main);
-    // 아이디 비밀번호 입력창 고정 및 비밀번호 암호 *로 표시
+    // 아이디 비밀번호 입력창 설정, 비밀번호 암호 *로 표시
+    jtf_userId.setForeground(Color.gray);
+    jtf_userPw.setForeground(Color.gray);
     jtf_userId.setBounds(60, 300, 270, 45);
-    jtf_userPw.setBounds(60, 340, 270, 45);
-    jtf_userPw.setEchoChar('*');
+    jtf_userPw.setBounds(60, 360, 270, 45);
+    // 입력창에 아무것도 없는 상태로 호출된 경우
+    if ("".equals(jtf_userId.getText())) {
+      jtf_userId.setText(" example@email.com");
+    }
+    if ("".equals(jtf_userPw.getText())) {
+      jtf_userPw.setText(" password");
+    }
+    // jtf 보더라인 설정
+    jtf_userId.setBorder(new LineBorder(Color.white, 8));
+    jtf_userPw.setBorder(new LineBorder(Color.white, 8));
+    // jtf_userPw.setEchoChar('*');
     // 로그인 버튼 정의
     jbtn_login.setBorderPainted(false);
     jbtn_login.setBackground(new Color(130, 65, 60));
     jbtn_login.setForeground(Color.white);
     jbtn_login.setFont(b14);
-    jbtn_login.setBounds(200, 400, 130, 45);
-    // KeyListener-엔터키 누르면 로그인 버튼 눌림
-    jbtn_login.addKeyListener(new KeyAdapter() {
-      @Override
-      public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-          // main = new Main(this); //메인화면 연결
-          // main.initDisplay();
-        }
-      }
-    });
+    jbtn_login.setBounds(200, 420, 130, 45);
     // 회원가입버튼 정의
     jbtn_join.setBorderPainted(false);
     jbtn_join.setBackground(new Color(130, 65, 60));
     jbtn_join.setForeground(Color.white);
     jbtn_join.setFont(b14);
-    jbtn_join.setBounds(60, 400, 130, 45);
+    jbtn_join.setBounds(60, 420, 130, 45);
     // 아이디/비밀번호 찾기 라벨버튼 정의
     jlb_findId.setText("<HTML><U>아이디 찾기</U></HTML>");
     jlb_findPw.setText("<HTML><U>비밀번호 찾기</U></HTML>");
@@ -122,10 +140,8 @@ public class Client extends JFrame implements ActionListener, MouseListener {
     jlb_findPw.setForeground(new ColorUIResource(135, 90, 75));
     jlb_findId.setFont(p12);
     jlb_findPw.setFont(p12);
-    jlb_findId.setBounds(100, 460, 70, 20);
-    jlb_findPw.setBounds(220, 460, 80, 20);
-    jlb_findId.addMouseListener(this);
-    jlb_findPw.addMouseListener(this);
+    jlb_findId.setBounds(100, 480, 70, 20);
+    jlb_findPw.setBounds(220, 480, 80, 20);
     // 바나나 이미지 정의
     jbtn_main.setBackground(new Color(255, 230, 120));
     jbtn_main.setBorderPainted(false); // 버튼 외곽선 없애기
@@ -150,9 +166,17 @@ public class Client extends JFrame implements ActionListener, MouseListener {
   public void actionPerformed(ActionEvent e) {
     Object obj = e.getSource();
     // 로그인 버튼을 눌렀을 때
-    if (jbtn_login == obj) {
+    if (obj == jbtn_login || obj == jtf_userId || obj == jtf_userPw) {
       userId = jtf_userId.getText();
       userPw = jtf_userPw.getText();
+      // 아이디를 입력하지 않았을 경우
+      if ("".equals(userId) || " example@email.com".equals(userId)) {
+        JOptionPane.showMessageDialog(jf_login, "이메일을 입력해주세요", "info", JOptionPane.WARNING_MESSAGE);
+      }
+      // 비밀번호를 입력하지 않았을 경우
+      else if ("".equals(userPw) || " password".equals(userPw)) {
+        JOptionPane.showMessageDialog(jf_login, "비밀번호를 입력해주세요", "info", JOptionPane.WARNING_MESSAGE);
+      }
       try {
         socket = new Socket("127.0.0.1", 3000);
         oos = new ObjectOutputStream(socket.getOutputStream());
@@ -168,7 +192,7 @@ public class Client extends JFrame implements ActionListener, MouseListener {
       }
     }
     // 회원가입 버튼을 눌렀을 때
-    else if (jbtn_join == obj) {
+    else if (obj == jbtn_join) {
       // memjoin = new Memjoin(this); //회원가입 연결
       // memjoin.initDisplay();
     }
@@ -182,12 +206,12 @@ public class Client extends JFrame implements ActionListener, MouseListener {
   public void mousePressed(MouseEvent e) {
     Object obj = e.getSource();
     // 아이디찾기 라벨 눌렀을 때
-    if (jlb_findId == obj) {
+    if (obj == jlb_findId) {
       idFind = new IdFind(this); // 아이디찾기 연결
       idFind.initDisplay();
     }
     // 비밀번호찾기 라벨 눌렀을 때
-    else if (jlb_findPw == obj) {
+    else if (obj == jlb_findPw) {
       pwFind = new PwFind(this); // 비밀번호찾기 연결
       pwFind.initDisplay();
     }
@@ -203,5 +227,31 @@ public class Client extends JFrame implements ActionListener, MouseListener {
 
   @Override
   public void mouseExited(MouseEvent e) {
+  }
+
+  @Override
+  public void focusGained(FocusEvent e) {
+    Object obj = e.getSource();
+    // 이름 jtf를 클릭했을 때
+    if (obj == jtf_userId) {
+      jtf_userId.setForeground(Color.black);
+      if (" example@email.com".equals(jtf_userId.getText())) {
+        jtf_userId.setText("");
+      }
+    }
+    // 핸드폰번호 jtf를 클릭했을 때
+    else if (obj == jtf_userPw) {
+      jtf_userPw.setForeground(Color.black);
+      if (" password".equals(jtf_userPw.getText())) {
+        jtf_userPw.setText("");
+      }
+    }
+
+  }
+
+  @Override
+  public void focusLost(FocusEvent e) {
+    // TODO Auto-generated method stub
+
   }
 }
