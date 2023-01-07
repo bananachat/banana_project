18a9eba7d8d78326a6e7626571b075df768987b2
EJ <temp221128@gmail.com>
2023-01-07 12:02:20 +0900

    01/07 로그 출력 기능 업데이트

--- app/src/main/java/banana_project/server/vo/LogVO.java
+++ app/src/main/java/banana_project/server/vo/LogVO.java
@@ -3,16 +3,18 @@ package banana_project.server.vo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Builder
 @AllArgsConstructor
-public class UserLogVO {
+@ToString
+public class LogVO {
     private String login_date;
     private int protocol;
     private String comments;
     private String user_id;
 
-    public UserLogVO() {
+    public LogVO() {
     }
 }
