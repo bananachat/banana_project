0b61247d04fdb2b8f9b374d0d3b9068b0687fb51
EJ <temp221128@gmail.com>
2023-01-15 11:29:17 +0900

    01/15 EncryptPassword 로그출력수정

--- app/src/main/java/banana_project/server/util/EncryptPassword.java
+++ app/src/main/java/banana_project/server/util/EncryptPassword.java
@@ -1,6 +1,7 @@
 package banana_project.server.util;
 
 import banana_project.server.logic.LogLogic;
+import banana_project.server.vo.LogVO;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -16,7 +17,7 @@ public class EncryptPassword {
      * @return sb.toString() 난수 리턴
      */
     public String getSalt() {
-        ll.writeLog(ConstantsLog.ENTER_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), null);
+        ll.writeLog(ConstantsLog.ENTER_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), new LogVO());
         //Random, byte 객체 생성
         SecureRandom ran = new SecureRandom();
         byte[] salt = new byte[20];
@@ -29,7 +30,7 @@ public class EncryptPassword {
         for (byte b : salt) {
             sb.append(String.format("%02x", b));
         }
-        ll.writeLog(ConstantsLog.EXIT_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), null);
+        ll.writeLog(ConstantsLog.EXIT_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), new LogVO());
         return sb.toString();
     }
 
@@ -41,7 +42,7 @@ public class EncryptPassword {
      * @return result 암호화된 비밀번호
      */
     public String getEncrypt(String password, String salt) {
-        ll.writeLog(ConstantsLog.ENTER_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), null);
+        ll.writeLog(ConstantsLog.ENTER_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), new LogVO());
         String result = "";
         try {
             //SHA256 알고리즘 객체 생성
@@ -60,7 +61,7 @@ public class EncryptPassword {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        ll.writeLog(ConstantsLog.EXIT_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), null);
+        ll.writeLog(ConstantsLog.EXIT_LOG, Thread.currentThread().getStackTrace()[1].getMethodName(), new LogVO());
         return result;
     }
 }
