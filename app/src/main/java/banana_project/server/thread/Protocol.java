11efc5c5efdaa29d2d1c9519a158fe6dc1a163d4
welsper53 <welsper53@gmail.com>
2023-01-16 20:24:39 +0900

    23.01.16 친구검색, 대화방화면 프로토콜 추가

--- app/src/main/java/banana_project/server/thread/Protocol.java
+++ app/src/main/java/banana_project/server/thread/Protocol.java
@@ -17,7 +17,7 @@ public class Protocol {
     public static final int EXIST_MAIL=202;     	//이메일 중복 존재
     public static final int NICK_CHK=203;	        //닉네임 중복 체크
     public static final int EXIST_NICK=204;	        //닉네임 중복 존재
-    public static final int ACNT_CHK=205;	        //계정(핸드폰번호) 중복 체크
+    public static final int ACNT_CHK=205;	        //계정(이름, 핸드폰번호) 중복 체크
     public static final int EXIST_ACNT=206;     	//계정 중복 존재
     public static final int SIGN_SUS=207;	        //회원가입 성공
     public static final int SIGN_ERR=208;	        //회원가입 실패
@@ -55,9 +55,11 @@ public class Protocol {
     public static final int	SRCH_USERS = 601; 	    //검색버튼(친구추가 → 모든 사용자 중에 검색)
     public static final int	PRT_FRIENDS = 602; 	    //친구검색 출력(새채팅 → 친구목록)
     public static final int	SRCH_FRIEDNDS = 603; 	//검색버튼(새채팅 → 친구 중에 검색)
-    public static final int	NF_RESULT = 604; 	    //검색 결과가 없음
+    public static final int	NF_RESULT = 604; 	    //친구 검색 결과가 없음
     public static final int	ADD_FRIEND = 605;   	//친구 추가 이벤트
-    public static final int	CREATE_CHAT = 606;  	//채팅방 만들기 이벤트
+    public static final int	CREATE_CHAT = 606;  	//채팅방 만들기 성공
+    public static final int	EXIST_FRIEND = 607;  	//친구 검색 존재
+    public static final int	FAIL_CRE_CHAT = 608;  	//채팅방 만들기 실패
 
 
     //          [대화방 화면]           //
@@ -69,4 +71,5 @@ public class Protocol {
     public static final int	ADD_MEM = 705; 	        //현 채팅방에 친구 초대(입장메세지)
     public static final int	EXIT_MEM = 706;     	//채팅방에서 나감(채팅방 삭제했을 때)
     public static final int	SAVE_CHAT = 707;    	//대화저장
+    public static final int	WRONG_NUM = 708;    	//잘못된 채팅방 번호
 }
