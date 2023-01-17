8330e9999b6c2d71586020c23b7309c171396dc4
welsper53 <welsper53@gmail.com>
2023-01-17 19:34:26 +0900

    23.01.17 런타임에러로 인해 사용중지(소스 수정)

--- app/src/main/java/banana_project/server/logic/SqlQuarry.java
+++ app/src/main/java/banana_project/server/logic/SqlQuarry.java
@@ -33,26 +33,26 @@ public class SqlQuarry {
         for (int i=0; i<columns.length-1; i++) {
             column += columns[i] + ", ";
         }
-        column = columns[columns.length-1];
+        column += columns[columns.length-1];
 
         // 밸류 파라미터 설정
         String value = "";
         for (int i=0; i<values.length-1; i++) {
             value += values[i] + ", ";
         }
-        value = values[values.length-1];
+        value += values[values.length-1];
 
 
-        StringBuilder sql = new StringBuilder();
-        sql.append("INSERT INTO ? ( ? ) VALUES ( ? ) ");
+        String sql = "INSERT INTO ? ( ? ) VALUES ( ? ) ";
 
         try {
             conn = dbMgr.getConnection();
-            pstmt = conn.prepareStatement(sql.toString());
+            pstmt = conn.prepareStatement(sql);
 
             pstmt.setString(1, table);
             pstmt.setString(2, column);
             pstmt.setString(3, value);
+
 
             // 쿼리 동작 레코드 수
             // 성공: 1 / 실패: 0
@@ -94,14 +94,14 @@ public class SqlQuarry {
         for (int i=0; i<columns.length-1; i++) {
             column += columns[i] + ", ";
         }
-        column = columns[columns.length-1];
+        column += columns[columns.length-1];
 
         // 변경할 값 파라미터 설정
         String cValue = "";
         for (int i=0; i<chgValues.length-1; i++) {
             cValue += chgValues[i] + ", ";
         }
-        cValue = chgValues[chgValues.length-1];
+        cValue += chgValues[chgValues.length-1];
 
         StringBuilder sql = new StringBuilder();
         sql.append("UPDATE ? SET ? = ? WHERE ? ");
@@ -212,21 +212,22 @@ public class SqlQuarry {
     } // end of quSelect (SELECT문 - 존재 유무)
 
 
+
     // TODO: select 메소드 작성 중 - 취소 가능성
     public List<Object> quSelect(String[] columns, String table, String whereClause) {
         // 리턴값
         List<Object> lResult = new ArrayList<>();
+        // 쿼리 결과값
+        List<Map> qResult = new ArrayList<>();
         // 쿼리결과 기본 false
         Boolean result = false;
 
         // 컬럼 파라미터 설정
         String column = "";
-        Map<String, Object> mCols = new HashMap<String, Object>();
         for (int i=0; i<columns.length-1; i++) {
             column += columns[i] + ", ";
-            mCols.put(columns[i], "");
         }
-        mCols.put(columns[columns.length-1], "");
+        column += columns[columns.length-1];
 
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT ? FROM ? WHERE ? ");
@@ -240,22 +241,21 @@ public class SqlQuarry {
             pstmt.setString(3, whereClause);
 
             // 쿼리 동작 레코드 수
-            // 성공: 1 / 실패: -1
+            // 성공: true / 실패: false
             result = pstmt.execute();
 
             rs = pstmt.executeQuery();
-            System.out.println("쿼리결과 : " + rs.toString());
 
+            // 결과값이 있을 경우
+            if(result) {
+                Map<String, Object> mCols = new HashMap<String, Object>();
 
-            for (int i=0; i<columns.length; i++) {
-                List<Map> lrow = new ArrayList<Map>();
-            }
-
-            while (rs.next()) {
-                for (int i=0; i<columns.length; i++) {
-                    Map<String, String> mColumn = new HashMap<String, String>();
-                    mColumn.put("columns[i]", rs.getString(columns[i]));
-//                    lrow.add(mColumn);
+                while (rs.next()) {
+                    // 컬럼 파라미터 설정
+                    for (int i=0; i<columns.length; i++) {
+                        mCols.put(columns[i], rs.getString(i));
+                    }
+                    qResult.add(mCols);
                 }
             }
 
@@ -273,6 +273,9 @@ public class SqlQuarry {
             }
         }
 
+
+        lResult.add(result);
+        lResult.add(qResult);
 
         return lResult;
     } // end of quSelect (SELECT문)
