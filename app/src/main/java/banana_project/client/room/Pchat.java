62a45140f1d00a620de4e45d36b90ef130d4955d
woojinyy <woojiny07@gmail.com>
2023-01-19 17:58:14 +0900

    0119채팅방

--- app/src/main/java/banana_project/client/room/Pchat.java
+++ app/src/main/java/banana_project/client/room/Pchat.java
@@ -1,7 +1,7 @@
 package banana_project.client.room;
 
 import javax.swing.*;
-import javax.swing.text.AbstractDocument.Content;
+import javax.swing.border.LineBorder;
 
 import banana_project.client.common.SetFontNJOp;
 import banana_project.client.common.SetImg;
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.CharArrayReader;
+import java.awt.Font;
 
 public class Pchat extends JFrame implements ActionListener {
     // 선언부
@@ -24,49 +24,64 @@ public class Pchat extends JFrame implements ActionListener {
     JLabel background = new JLabel(new ImageIcon("room_back.png"));
 
     // 채팅창
+    ImageIcon img = new ImageIcon("./app\\src\\main\\java\\banana_project\\image\\send2.png");
     JTextArea chatArea = new JTextArea();
     JTextField jtf = new JTextField(24);
     JButton jbtn_send = new JButton("보내기");
     JButton jbtn_back = new JButton("이전");
     JPanel jp_Pchat = new JPanel(null);
-
-    // 스크롤바
-    JViewport viewport = new JViewport();
-    
+    JPanel jp_center = new JPanel(null);
     String message;
+    JScrollPane jsp_display = new JScrollPane(jp_center, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
     public Pchat() {// 디폴트생성자
     }
 
     // 화면그리기
     public void initDisplay() {
-        JScrollPane jsp = new JScrollPane( JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
         // 이벤트
         jbtn_send.addActionListener(this);
         jbtn_back.addActionListener(this);
 
         // NORTH////
-        jbtn_back.setBounds(300, 30, 60, 30);
-        jp_Pchat.add(jbtn_back);
         jbtn_back.setBorderPainted(false);
+        jbtn_back.setBackground(new Color(130, 65, 60));
+        jbtn_back.setForeground(Color.white);
+        jbtn_back.setFont(setFontNJOp.b14);
+        jbtn_back.setBounds(30, 30, 70, 30);
+        // jbtn_back.setRolloverIcon(img);
+        jbtn_back.setBorderPainted(false);
+        jp_Pchat.add(jbtn_back);
         jbtn_back.setBackground(new Color(130, 65, 60));
         jbtn_back.setForeground(Color.WHITE);
 
         // SOUTH///
-        jtf.setBounds(15, 495, 295, 30);
-        jbtn_send.setBounds(310, 495, 70, 30);
+        jtf.setBounds(21, 495, 285, 30);
+        jtf.setBorder(new LineBorder(Color.white, 8));
+        jtf.setBorder(null);
+        jtf.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+        jbtn_send.setBorderPainted(false);
         jbtn_send.setBackground(new Color(130, 65, 60));
+        jbtn_send.setForeground(Color.white);
+        jbtn_send.setFont(setFontNJOp.b14);
+        jbtn_send.setBounds(305, 495, 78, 30);
+        jbtn_send.setBackground(new Color(130, 65, 60));
+        jbtn_send.setPreferredSize(new Dimension(70, 30));
         jbtn_send.setForeground(Color.WHITE);
         jbtn_send.setBorderPainted(false);
         jp_Pchat.add(jtf);
         jp_Pchat.add(jbtn_send);
 
         // CENTER///
-        chatArea.setBackground(new Color(255, 230, 120));
-        chatArea.setBounds(3, 80, 350, 400);
-        jp_Pchat.add(chatArea);
+        jsp_display.setBounds(20, 95, 350, 400);
+        jsp_display.getVerticalScrollBar().setUnitIncrement(16);
+        jp_center.add(chatArea);
+        chatArea.setBackground(Color.white);
+        chatArea.setBounds(1, 1, 350, 500);
+        jp_Pchat.add(jsp_display);
+        this.add(jp_Pchat);
         
         /// 기본값//
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,19 +92,23 @@ public class Pchat extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setContentPane(jp_Pchat);
-        this.add(jsp);
+
         // this.add(jp_Pchat);
         jtf.addKeyListener(new KeyAdapter() {// 엔터키 입력
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     message = jtf.getText();
-                    chatArea.append(message + "\n");
+                    String text = chatArea.getText();
+                    String[] words = text.split("//s");
                     jtf.setText("");
+                    chatArea.append("user.id : "+message + "\n");
+
                     // System.out.println("엔터키입력");
                 }
             }
         });
+
         jbtn_send.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -103,6 +122,7 @@ public class Pchat extends JFrame implements ActionListener {
                 }
             }
         });
+
     }// 화면 그리기 끝
 
     public static void main(String[] args) {
@@ -113,7 +133,7 @@ public class Pchat extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e1) {
-        // TODO Auto-generated method stub
+
         Object obj = e1.getSource();
         if (obj == jbtn_back) {
 
